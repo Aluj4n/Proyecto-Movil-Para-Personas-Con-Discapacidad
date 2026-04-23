4f5162e0a37fbc3ba7e8c204770a4652a85746c5
Angelo <angelo10191817@gmail.com>
2026-04-23 13:39:04 -0500

    Se agrego menu opciones en barra superior para el cierre de sesion google auth.

--- app/src/main/java/com/example/proyectomovilparapersonascondiscapacidad/Inicio.kt
+++ app/src/main/java/com/example/proyectomovilparapersonascondiscapacidad/Inicio.kt
@@ -7,6 +7,17 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+
+//Import's agregados -v
+import android.content.Intent
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+
+
 class Inicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +28,14 @@ class Inicio : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Menu opciones
+        val toolbar = findViewById<Toolbar>(R.id.menuOpciones)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "FocusEco"
+
+
+        //Navigation view
         val bottom=findViewById<BottomNavigationView>(R.id.bottomNavigation)
         supportFragmentManager.beginTransaction().replace(R.id.contenedor, TareasFragment()).commit()
 
@@ -38,4 +57,32 @@ class Inicio : AppCompatActivity() {
             }
         }
     }
+    
+    //Logica para cierre de sesion Google
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_options, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.cerrarGoogle -> {
+                cerrarSesion()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+    private fun cerrarSesion() {
+        FirebaseAuth.getInstance().signOut()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        googleSignInClient.signOut().addOnCompleteListener {
+            // 3. Regresar al Login (MainActivity)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
 }
