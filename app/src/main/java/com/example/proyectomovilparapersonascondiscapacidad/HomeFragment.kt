2ef8c924e0f66629357128e8637beaf7c1a181da
Angelo <angelo10191817@gmail.com>
2026-05-09 17:43:14 -0500

    Implementacion de mensajes motivacionales en homeFragment

--- app/src/main/java/com/example/proyectomovilparapersonascondiscapacidad/HomeFragment.kt
+++ app/src/main/java/com/example/proyectomovilparapersonascondiscapacidad/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class HomeFragment : Fragment() {
 
@@ -14,4 +15,37 @@ class HomeFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.homefragment, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Referenciamos el textview tvfrase de homefragment.xml
+        val tvFrase = view.findViewById<TextView>(R.id.tvFrase)
+
+        //Creamos una lista y agregamos frases
+        val frases = listOf(
+            "No necesitas hacerlo perfecto, solo dar el siguiente paso.",
+            "Tu forma de pensar es diferente, y eso también puede ser una fortaleza.",
+            "Está bien avanzar poco a poco; el progreso sigue siendo progreso.",
+            "No eres distraído por falta de capacidad, tu mente simplemente funciona de otra manera.",
+            "Descansar también es parte del proceso.",
+            "Cada pequeña tarea terminada cuenta.",
+            "Tu creatividad y energía tienen mucho valor.",
+            "No te compares con el ritmo de los demás.",
+            "Puedes reorganizarte las veces que necesites.",
+            "Un mal día no define todo tu esfuerzo.",
+            "Lo importante no es cuánto tardas, sino que sigues intentándolo.",
+            "Tu potencial no se mide por tu nivel de concentración.",
+            "Está bien pedir ayuda cuando la necesites.",
+            "Las metas grandes también se logran con pasos pequeños.",
+            "Tu mente puede ser caótica a veces, pero también muy brillante."
+        )
+
+        //
+        val fraseAleatoria = frases.random()
+        tvFrase.text="\"$fraseAleatoria\""
+    }
+
+
+
 }
